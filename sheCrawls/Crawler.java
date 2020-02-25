2d2302dcc3a6d619851266e33452c13fa1740ac9
Mora <mlabisi98@gmail.com>
2020-02-24 16:31:42 -0800

    finished extractContent method

--- sheCrawls/Crawler.java
+++ sheCrawls/Crawler.java
@@ -2,76 +2,87 @@
  * Crawler object class.
  */
 package sheCrawls;
+
 import java.io.*;
 import java.util.ArrayList;
+
 import org.jsoup.Jsoup; //import core public access point for jsoup
 import org.jsoup.nodes.Document; //HTML document 
 import org.jsoup.nodes.Element; //HTML element - extract data, manipulate HTML
 import org.jsoup.select.Elements; //list of elements 
 
 public class Crawler {
-	private Document doc;
-	private String language;
-	private ArrayList<String> links = new ArrayList<String>();
-	
-	//constructor takes URL and desired language 
-	public Crawler(String url, String lang){
-		try {
-			this.doc = Jsoup.connect(url).get();
-			this.language = lang;
-			downloadContent(url);
-		}
-		
-		catch(Exception e) {
-			System.out.println("she can't crawl this URL.");
-		}
-	}
-	
-	private Boolean checkLang(Document d) {
-		Element language = doc.select("html").first();
-		String docLang = language.attr("lang");
-		System.out.println(docLang);
+    private String language;
+    private ArrayList<String> links = new ArrayList<String>();
 
-		return docLang.equals(this.language);
-	}
-	
-	//get links, count links 
-	//take in a URL 
-	private void downloadContent(String url) {
-		int linkCount = 0;
+    //constructor takes URL and desired language
+    public Crawler(String url, String lang) {
+        try {
+            this.language = lang;
+            downloadContent(url);
+        } catch (Exception e) {
+            System.out.println("she can't crawl this URL.");
+        }
+    }
 
-		try {
-			//convert to doc
-			Document d = Jsoup.connect(url).get();
+    private Boolean checkLang(Document doc) {
+        Element language = doc.select("html").first();
+        String docLang = language.attr("lang");
+        System.out.println(docLang);
 
-			//check language
-			if (checkLang(d)) {
-				//download ALL the content, place in Repo folder
-				extractContent(d);
+        return docLang.equals(this.language);
+    }
 
-				//search for all outlinks
+    //get links, count links
+    //take in a URL
+    private void downloadContent(String url) {
+        int linkCount = 0;
+        Document d;
 
-				//add URL + linkCount to report.csv
-				addCSVEntry("");
+        try {
+            //convert to doc
+            d = Jsoup.connect(url).get();
 
-				//call downloadContent on all outlinks
-			}
-		}
-		
-		catch (Exception e) {
-			System.out.println("Couldn't write to file");
-		}	
-	}
-	
-	private void getLinks(String URL) {
-		//arraylist
-	}
+            //check language
+            if (checkLang(d)) {
+                //download ALL the content, place in repository directory
+                extractContent(d);
 
-	private void extractContent(Document doc) {
+                //search for all outlinks
 
-	}
+                //add URL + linkCount to report.csv
+                addCSVEntry("");
 
-	private void addCSVEntry(String entry) {
+                //call downloadContent on all outlinks
+            }
+        } catch (Exception e) {
+            System.out.println("Couldn't write to file");
+        }
+    }
 
-	}
+    private void getLinks(String URL) {
+        //arraylist
+    }
+
+    private void extractContent(Document d) {
+        String dirName = System.getProperty("user.dir") + "/repository";
+        String fileName = d.title().replace(" ", "");
+        String fileExt = ".txt";
+        String docText = d.text();
+
+        File dir = new File(dirName);
+        dir.mkdir();
+
+        File file = new File(dirName + File.separator + fileName + fileExt);
+        try (FileWriter fw = new FileWriter(file.getAbsoluteFile());
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write(docText);
+        } catch (IOException e) {
+            System.out.println("Couldn't write to file");
+        }
+    }
+
+    private void addCSVEntry(String entry) {
+
+    }
 } //end Crawler
