8a0d7aedcb8c60509ec7db4a8759c39c18345dcb
Mora <mlabisi98@gmail.com>
2020-02-26 13:14:49 -0800

    added language detection -- crawler works!

--- sheCrawls/Crawler.java
+++ sheCrawls/Crawler.java
@@ -5,87 +5,78 @@ package sheCrawls;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.jsoup.Jsoup; //import core public access point for jsoup
-import org.jsoup.nodes.Document; //HTML document 
+import org.jsoup.nodes.Document; //HTML document
 import org.jsoup.nodes.Element; //HTML element - extract data, manipulate HTML
 import org.jsoup.select.Elements;
 import org.jsoup.safety.Whitelist;
 
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+
 public class Crawler {
     private String language;
-    private ArrayList<String> linksList = new ArrayList<String>();
-    int linkCount = 0;
+    private ArrayList<String> links;
 
-    //constructor takes URL and desired language
+
     public Crawler(String url, String lang) {
+        this.language = lang;
+        this.links = new ArrayList<>();
+
+        this.links.add(url);
+
+        crawl(url, 0);
+    }
+
+    private void crawl(String url, int crawlCt) {
         try {
-            this.language = lang;
-            downloadContent(url);
+            Document d = Jsoup.connect(url).get();
+            ArrayList<String> outlinks;
+            System.out.println();
+
+            if (checkLang(d)) {
+                crawlCt++;
+
+                exportContent(d);
+                outlinks = getPageLinks(d);
+                addCSVEntry(this.language, url, outlinks.size());
+
+
+                for (int i = 0; i < Math.min(outlinks.size(), 10) && links.size() < 150; i++) {
+                    String outlink = outlinks.get(i);
+                    for (int j = -1; j < crawlCt; j++) {
+                        System.out.print("\t");
+                    }
+                    System.out.print(">  (Outlink #" + (i+1) + " of " + outlinks.size() + "):\t" + outlink );
+                    crawl(outlink, crawlCt);
+                }
+            }
         } catch (Exception e) {
-            System.out.println("she can't crawl this URL.");
+            System.out.println(e.getMessage());
         }
     }
 
     private Boolean checkLang(Document doc) {
+        String docLang = detect(doc.body().text());
+
         Element language = doc.select("html").first();
-        String docLang = language.attr("lang");
-        System.out.println(docLang);
+        String encoding = language.attr("lang");
 
-        return docLang.equals(this.language);
+        return docLang.equalsIgnoreCase(this.language) && docLang.equalsIgnoreCase(encoding);
     }
 
-    //get links, count links
-    //take in a URL
-    private void downloadContent(String url) {
+    private String detect(String text) {
+       try {
+           Detector detector = DetectorFactory.create();
+           detector.append(text);
+           return detector.detect();
+       } catch (LangDetectException e) {
+           System.out.println(e.getMessage());
+       }
 
-        try {
-            //convert to doc
-            Document d = Jsoup.connect(url).get();
-
-            //check language
-            if (checkLang(d)) {
-                //download ALL the content, place in repository directory
-                exportContent(d);
-
-                //search for all outlinks
-                getPageLinks(url);
-
-                //add URL + linkCount to report.csv
-                addCSVEntry(this.language, d.location(), linkCount);
-                //call downloadContent on all outlinks
-                if (linkCount <= 109){
-                    String webLink = linksList.get(linkCount);
-                    linkCount = linkCount + 1;
-                    System.out.println("Size of array: " + linksList.size());
-                    System.out.println(linkCount);
-                    downloadContent(webLink);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Couldn't write to file");
-        }
-    }
-
-    //Check if URL has already been crawled and/or if we've seen the same content before
-    public void getPageLinks(String url) {
-        if (!linksList.contains(url) && (linksList.size() <= 109)) {
-            try {
-                linksList.add(url);
-                System.out.println("URL: " + url);
-                System.out.println("Number of links: " + linksList.size());
-
-                Document document = Jsoup.connect(url).get();
-                Elements linksOnPage = document.select("a[href]");
-
-                for (Element page : linksOnPage) {
-                    getPageLinks(page.attr("abs:href"));
-                }
-            } catch (IOException e) {
-                System.err.println("For '" + url + "': " + e.getMessage());
-            }
-        }
+       return "fail";
     }
 
     private void exportContent(Document d) {
@@ -101,8 +92,33 @@ public class Crawler {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()))) {
             bw.write(docHTML);
         } catch (IOException e) {
-            System.out.println("Couldn't write to " + fileName);
+            System.out.println(e.getMessage());
         }
+    }
+
+    private ArrayList<String> getPageLinks(Document d) {
+        String dUrl = d.location();
+        ArrayList<String> outlinks = new ArrayList<>();
+        try {
+            Elements urls = d.select("a[href]");
+
+            for (Element url : urls) {
+                String href = url.attr("href");
+                if (!links.contains(href) && !outlinks.contains(href) && isValid(href)) {
+                    links.add(href);
+                    outlinks.add(href);
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.print("For '" + dUrl + "': " + e.getMessage());
+        }
+
+        return outlinks;
+    }
+
+    private boolean isValid(String href) {
+        return !(href.startsWith("/") || href.isBlank() || href.startsWith("?") || href.startsWith("./") || href.startsWith("../") || href.startsWith("#") || href.startsWith("tel:"));
     }
 
     private void addCSVEntry(String lang, String url, int ct) {
